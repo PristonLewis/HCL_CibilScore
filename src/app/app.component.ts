2764e1967c423b9388c8f3b77a4035930a192eb4
user1 <user1@LP-5CD9296CX3.HCLT.CORP.HCL.IN>
2020-01-28 16:24:57 +0530

    signout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employee-statistic';
+  constructor(private auth: AuthService, private router: Router){}
+  public signout(){
+    localStorage.setItem("auth", "false")
+    localStorage.setItem("user", "")
+    // this.auth.changeAuth("Login");
+    this.router.navigate(['/login']);
+  }
 }
