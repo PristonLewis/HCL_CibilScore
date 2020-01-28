bd7b0419a3ccbb91ac070c48b57f4a6fde4c37ee
nagashayanks <user1@LP-5CD9176M8P.HCLT.CORP.HCL.IN>
2020-01-28 15:00:55 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './module/home/home.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
